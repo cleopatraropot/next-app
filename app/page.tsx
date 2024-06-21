a1ad638f41b5c1a6df31e2d78c997cb102395290
cleopatraropot <cleopatraropot@gmail.com>
2024-06-21 12:23:14 +0300

    Client and server components

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import ProductCard from "@/app/components/ProductCard";
 
 export default function Home() {
   return (
     <main>
       <h1>Hello world</h1>
       <a href="/users">Users</a>
+        <ProductCard/>
     </main>
   );
 }
